4c2ca2128e027724affe06b0073487f38e49dfa5
Aran Borkum <aranborkum@hotmail.com>
2019-01-23 17:12:49 +0000

    making a lite version of the game

---
+++ src/ComaLite.cxx
@@ -0,0 +1,19 @@
+// -----------------------------
+// COMA SIMULATOR version 0.0.1
+// Authors: A. Borkum, M. Nirkko
+// Updated: 21 Jan 2019
+// -----------------------------
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  
+
+    system("gio open images/black.png");
+  
+  return 0;
+  
+}
