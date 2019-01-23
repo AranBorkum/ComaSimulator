2a350538b660f96c944ed3dbb4cf51a3810f4715
mnirkko <m.nirkko@sussex.ac.uk>
2019-01-23 16:56:33 +0000

    Move source code to subdirectory.

---
+++ src/Coma.cxx
@@ -0,0 +1,36 @@
+// -----------------------------
+// COMA SIMULATOR version 0.0.1
+// Authors: A. Borkum, M. Nirkko
+// Updated: 21 Jan 2019
+// -----------------------------
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  
+  srand(time(NULL));
+  int i = 0;
+  int d = rand() % 10 + 1;
+  char input[100];
+  
+  while(i<d) {
+    std::cout << "You are in a coma." << std::endl;
+    std::cout << "What would you like to do? ";
+    std::cin.getline(input,sizeof(input));
+    std::cout << "I'm sorry. You can't " << input << "." << std::endl;
+    i++;
+  }
+
+  if (d == 7) {
+    system("gio open images/lucky7.png");
+  } else {
+    std::cout << "You can't even. You die after " << i << " attempts." << std::endl;
+    system("gio open images/youDied.jpg");
+  }
+  
+  return 0;
+  
+}
